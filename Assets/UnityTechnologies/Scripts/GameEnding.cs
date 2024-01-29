1c605ab9f5f4e700b3e3cb39c23d924af9703b60
Rubiio14 <147033219+Rubiio14@users.noreply.github.com>
2024-01-29 17:23:55 +0000

    Jhon_Lemon_Finished

--- Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
-   public float fadeDuration = 1f;
-   public float displayImageDuration = 1f;
-   public GameObject player;
-   public CanvasGroup exitBackgroundImageCanvasGroup;
-   public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+	public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
     bool m_IsPlayerCaught;
     float m_Timer;
+    bool m_HasAudioPlayed;
 
-   void OnTriggerEnter (Collider other)
+    void OnTriggerEnter (Collider other)
    {
 	   if(other.gameObject == player)
 	   {
@@ -32,18 +35,23 @@ public class GameEnding : MonoBehaviour
    {
 		if (m_IsPlayerAtExit)
 		{
-			EndLevel(exitBackgroundImageCanvasGroup, false);
+			EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
 		}
 		else if (m_IsPlayerCaught)
 		{
-			EndLevel(caughtBackgroundImageCanvasGroup, true);
+			EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
 		}
    }
 
-   void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+   void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
    {
-	   m_Timer += Time.deltaTime;
-
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+        
+        m_Timer += Time.deltaTime;
 	   imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
 	   if(m_Timer > fadeDuration + displayImageDuration)
